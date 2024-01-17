7e7e23c7a216573f1e4254e562ff3de4d615cb82
vikaschavan12 <vikas.chavan@sumasoft.net>
2024-01-17 13:37:21 +0530

    inital commit

---
+++ src/main/java/Mahindra_Finance_JD/MahindraFinanceJdApplication.java
@@ -0,0 +1,13 @@
+package Mahindra_Finance_JD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MahindraFinanceJdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MahindraFinanceJdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Mahindra_Finance_JD/MahindraFinanceJdApplicationTests.java
@@ -0,0 +1,13 @@
+package Mahindra_Finance_JD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MahindraFinanceJdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
